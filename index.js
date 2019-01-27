d6d4058b4e0ad25cf75629354c2ced421f9477b7
qxx3184 <luca.davino1996@gmail.com>
2019-01-27 08:59:57 +0100

    b

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.post('/', (req,res) => {
     res.json(req.body);
 })
 
-app.listen(3000, () => console.log('app listening on port 3000'))
+app.listen(3000, () => console.log('app listening on p 3000'))
