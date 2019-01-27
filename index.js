9d0d2c9068ccfc65ad1d2a1b7b30bd2664b1e8c0
qxx3184 <luca.davino@gigatronik.com>
2019-01-27 08:48:41 +0100

    TEST

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+app.get('/', (req,res) => {
+    console.log(req.body)
+    res.json(req.body);
+});
+
+app.post('/', (req,res) => {
+    console.log(req.body)
+    res.json(req.body);
+})
+
+app.listen(3000, () => console.log('app listening on port 3000'))
